94059b3bb6c7fddb5cfe61edc3a3256b3679b4e2
ozan-arslan <arslanozan.arb@gmail.com>
2024-01-31 04:10:17 +0300

    I shouldn't have tried recursive in the first place, sigh. Roman numeral converter is done.

--- Build a Roman Numeral Converter/script.js
+++ Build a Roman Numeral Converter/script.js
@@ -3,7 +3,29 @@ const output = document.getElementById("output");
 const convertBtn = document.getElementById("convert-btn");
 
 const compute = (num) => {
-  console.log(num);
+  output.innerText = "";
+  const roman = [
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "XC",
+    "L",
+    "XL",
+    "X",
+    "IX",
+    "V",
+    "IV",
+    "I",
+  ];
+  const arabic = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  for (let i = 0; i < arabic.length; i++) {
+    while (arabic[i] <= num) {
+      num -= arabic[i];
+      output.innerText += roman[i];
+    }
+  }
 };
 
 convertBtn.addEventListener("click", (event) => {
@@ -16,6 +38,6 @@ convertBtn.addEventListener("click", (event) => {
   } else if (onlyIntegers[0] > 3999) {
     output.innerText = "Please enter a number less than or equal to 3999";
   } else if (onlyIntegers > 0 && onlyIntegers < 4000) {
-    compute(onlyIntegers);
+    compute(parseInt(onlyIntegers[0]));
   }
 });
