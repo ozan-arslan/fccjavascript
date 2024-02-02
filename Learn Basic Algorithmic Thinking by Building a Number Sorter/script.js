525bfe8a84f671e21fdfec889bc328047f48a93c
ozan-arslan <arslanozan.arb@gmail.com>
2024-02-02 17:22:58 +0300

    .

--- Learn Basic Algorithmic Thinking by Building a Number Sorter/script.js
+++ Learn Basic Algorithmic Thinking by Building a Number Sorter/script.js
@@ -7,7 +7,9 @@ const sortInputArray = (event) => {
     ...document.getElementsByClassName("values-dropdown"),
   ].map((dropdown) => Number(dropdown.value));
 
-  updateUI(inputValues);
+  const sortedValues = bubbleSort(inputValues);
+
+  updateUI(sortedValues);
 };
 
 const updateUI = (array = []) => {
@@ -15,6 +17,22 @@ const updateUI = (array = []) => {
     const outputValueNode = document.getElementById(`output-value-${i}`);
     outputValueNode.innerText = num;
   });
+};
+
+const bubbleSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - 1; j++) {
+      console.log(array, array[j], array[j + 1]);
+
+      if (array[j] > array[j + 1]) {
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+
+  return array;
 };
 
 sortButton.addEventListener("click", sortInputArray);
