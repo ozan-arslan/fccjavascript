451a1b2c0ff7e2e3a9ebc938b6b18f1a085e235b
ozan-arslan <arslanozan.arb@gmail.com>
2024-02-06 04:20:21 +0300

    this was frustrating. I'll have a lot of time with algorithms in the future

--- Learn Basic Algorithmic Thinking by Building a Number Sorter/script.js
+++ Learn Basic Algorithmic Thinking by Building a Number Sorter/script.js
@@ -1,6 +1,23 @@
-const sortButton = document.getElementById("sort");
+const bubbleSortButton = document.getElementById("bubble-sort");
+const selectionSortButton = document.getElementById("selection-sort");
+const insertionSortButton = document.getElementById("insertion-sort");
+const sortMethodButton = document.getElementById("sort-method");
 
-const sortInputArray = (event) => {
+const sortMethod = (event) => {
+  event.preventDefault();
+
+  const inputValues = [
+    ...document.getElementsByClassName("values-dropdown"),
+  ].map((dropdown) => Number(dropdown.value));
+
+  const sortedValues = inputValues.sort((a, b) => {
+    return a - b;
+  });
+
+  updateUI(sortedValues);
+};
+
+const bubbleSortInputArray = (event) => {
   event.preventDefault();
 
   const inputValues = [
@@ -8,6 +25,30 @@ const sortInputArray = (event) => {
   ].map((dropdown) => Number(dropdown.value));
 
   const sortedValues = bubbleSort(inputValues);
+
+  updateUI(sortedValues);
+};
+
+const selectionSortInputArray = (event) => {
+  event.preventDefault();
+
+  const inputValues = [
+    ...document.getElementsByClassName("values-dropdown"),
+  ].map((dropdown) => Number(dropdown.value));
+
+  const sortedValues = selectionSort(inputValues);
+
+  updateUI(sortedValues);
+};
+
+const insertionSortInputArray = (event) => {
+  event.preventDefault();
+
+  const inputValues = [
+    ...document.getElementsByClassName("values-dropdown"),
+  ].map((dropdown) => Number(dropdown.value));
+
+  const sortedValues = insertionSort(inputValues);
 
   updateUI(sortedValues);
 };
@@ -22,8 +63,6 @@ const updateUI = (array = []) => {
 const bubbleSort = (array) => {
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length - 1; j++) {
-      console.log(array, array[j], array[j + 1]);
-
       if (array[j] > array[j + 1]) {
         const temp = array[j];
         array[j] = array[j + 1];
@@ -35,4 +74,39 @@ const bubbleSort = (array) => {
   return array;
 };
 
-sortButton.addEventListener("click", sortInputArray);
+const selectionSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    const temp = array[i];
+    array[i] = array[minIndex];
+    array[minIndex] = temp;
+  }
+
+  return array;
+};
+
+const insertionSort = (array) => {
+  for (let i = 1; i < array.length; i++) {
+    const currValue = array[i];
+    let j = i - 1;
+
+    while (j >= 0 && array[j] > currValue) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = currValue;
+  }
+  return array;
+};
+
+sortMethodButton.addEventListener("click", sortMethod);
+bubbleSortButton.addEventListener("click", bubbleSortInputArray);
+selectionSortButton.addEventListener("click", selectionSortInputArray);
+insertionSortButton.addEventListener("click", insertionSortInputArray);
