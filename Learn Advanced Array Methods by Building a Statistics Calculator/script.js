42df28327b6dda6c242b9c33f006423fd12ed4e7
ozan-arslan <arslanozan.arb@gmail.com>
2024-02-08 00:49:46 +0300

    updating progress

---
+++ Learn Advanced Array Methods by Building a Statistics Calculator/script.js
@@ -0,0 +1,12 @@
+const getMean = (array) =>
+  array.reduce((acc, el) => acc + el, 0) / array.length;
+
+const calculate = () => {
+  const value = document.querySelector("#numbers").value;
+  const array = value.split(/,\s*/g);
+  const numbers = array.map((el) => Number(el)).filter((el) => !isNaN(el));
+
+  const mean = getMean(numbers);
+
+  document.querySelector("#mean").textContent = mean;
+};
