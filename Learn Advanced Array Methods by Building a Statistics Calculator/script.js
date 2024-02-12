b65de86a2a8902b7776675619e93e360a296eb19
ozan-arslan <arslanozan.arb@gmail.com>
2024-02-13 02:01:49 +0300

    it was a frustrating step but beneficial.

--- Learn Advanced Array Methods by Building a Statistics Calculator/script.js
+++ Learn Advanced Array Methods by Building a Statistics Calculator/script.js
@@ -1,12 +1,23 @@
 const getMean = (array) =>
   array.reduce((acc, el) => acc + el, 0) / array.length;
 
+const getMedian = (array) => {
+  const sorted = array.sort((a, b) => a - b);
+  const median =
+    array.length % 2 === 0
+      ? getMean([sorted[array.length / 2], sorted[array.length / 2 - 1]])
+      : sorted[Math.floor(array.length / 2)];
+  return median;
+};
+
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
   const numbers = array.map((el) => Number(el)).filter((el) => !isNaN(el));
 
   const mean = getMean(numbers);
+  const median = getMedian(numbers);
 
   document.querySelector("#mean").textContent = mean;
+  document.querySelector("#median").textContent = median;
 };
