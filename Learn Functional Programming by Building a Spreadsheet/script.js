4a14a8d25ce9702b8f5a172a8645c3104f5fa8b5
ozan-arslan <arslanozan.arb@gmail.com>
2024-03-06 01:18:44 +0300

    more array manipulation

---
+++ Learn Functional Programming by Building a Spreadsheet/script.js
@@ -0,0 +1,21 @@
+const range = (start, end) =>
+  Array(end - start + 1)
+    .fill(start)
+    .map((element, index) => element + index);
+const charRange = (start, end) =>
+  range(start.charCodeAt(0), end.charCodeAt(0)).map((code) =>
+    String.fromCharCode(code)
+  );
+
+window.onload = () => {
+  const container = document.getElementById("container");
+  const createLabel = (name) => {
+    const label = document.createElement("div");
+    label.className = "label";
+    label.textContent = name;
+    container.appendChild(label);
+  };
+  const letters = charRange("A", "J");
+  letters.forEach(createLabel);
+  range(1, 99).forEach((number) => {});
+};
