ff033d3b9f5ca5a4803949c170f610e1e1f8d8db
ozan-arslan <arslanozan.arb@gmail.com>
2024-03-16 20:11:39 +0300

    progress

--- Learn Functional Programming by Building a Spreadsheet/script.js
+++ Learn Functional Programming by Building a Spreadsheet/script.js
@@ -11,6 +11,12 @@ const median = (nums) => {
     : sorted[Math.ceil(middle)];
 };
 
+const spreadsheetFunctions = {
+  sum,
+  average,
+  median,
+};
+
 const range = (start, end) =>
   Array(end - start + 1)
     .fill(start)
@@ -19,6 +25,24 @@ const charRange = (start, end) =>
   range(start.charCodeAt(0), end.charCodeAt(0)).map((code) =>
     String.fromCharCode(code)
   );
+
+const evalFormula = (x, cells) => {
+  const idToText = (id) => cells.find((cell) => cell.id === id).value;
+  const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
+  const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
+  const elemValue = (num) => (character) => idToText(character + num);
+  const addCharacters = (character1) => (character2) => (num) =>
+    charRange(character1, character2).map(elemValue(num));
+  const rangeExpanded = x.replace(
+    rangeRegex,
+    (_match, char1, num1, char2, num2) =>
+      rangeFromString(num1, num2).map(addCharacters(char1)(char2))
+  );
+  const cellRegex = /[A-J][1-9][0-9]?/gi;
+  const cellExpanded = rangeExpanded.replace(cellRegex, (match) =>
+    idToText(match.toUpperCase())
+  );
+};
 
 window.onload = () => {
   const container = document.getElementById("container");
@@ -37,7 +61,15 @@ window.onload = () => {
       input.type = "text";
       input.id = letter + number;
       input.ariaLabel = letter + number;
+      input.onchange = update;
       container.appendChild(input);
     });
   });
+};
+
+const update = (event) => {
+  const element = event.target;
+  const value = element.value.replace(/\s/g, "");
+  if (!value.includes(element.id) && value.startsWith("=")) {
+  }
 };
