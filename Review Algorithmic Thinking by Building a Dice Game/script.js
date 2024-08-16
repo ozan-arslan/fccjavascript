7cad54ec15d47015e0a33e4e4a34fdb7e250d2f0
ozan-arslan <arslanozan.arb@gmail.com>
2024-08-17 02:17:40 +0300

    Unlike other courses, this one actually made me write the code. Especially checkForStraights function.

--- Review Algorithmic Thinking by Building a Dice Game/script.js
+++ Review Algorithmic Thinking by Building a Dice Game/script.js
@@ -1,9 +1,9 @@
 const listOfAllDice = document.querySelectorAll(".die");
 const scoreInputs = document.querySelectorAll("#score-options input");
 const scoreSpans = document.querySelectorAll("#score-options span");
-const currentRound = document.getElementById("current-round");
-const currentRoundRolls = document.getElementById("current-round-rolls");
-const totalScore = document.getElementById("total-score");
+const roundElement = document.getElementById("current-round");
+const rollsElement = document.getElementById("current-round-rolls");
+const totalScoreElement = document.getElementById("total-score");
 const scoreHistory = document.getElementById("score-history");
 const rollDiceBtn = document.getElementById("roll-dice-btn");
 const keepScoreBtn = document.getElementById("keep-score-btn");
@@ -13,7 +13,6 @@ const rulesBtn = document.getElementById("rules-btn");
 let diceValuesArr = [];
 let isModalShowing = false;
 let score = 0;
-let total = 0;
 let round = 1;
 let rolls = 0;
 
@@ -31,8 +30,8 @@ const rollDice = () => {
 };
 
 const updateStats = () => {
-  currentRoundRolls.textContent = rolls;
-  currentRound.textContent = round;
+  rollsElement.textContent = rolls;
+  roundElement.textContent = round;
 };
 
 const updateRadioOption = (index, score) => {
@@ -41,13 +40,157 @@ const updateRadioOption = (index, score) => {
   scoreSpans[index].textContent = `, score = ${score}`;
 };
 
+const updateScore = (selectedValue, achieved) => {
+  score += parseInt(selectedValue);
+  totalScoreElement.textContent = score;
+
+  scoreHistory.innerHTML += `<li>${achieved} : ${selectedValue}</li>`;
+};
+
+const getHighestDuplicates = (arr) => {
+  const counts = {};
+
+  for (const num of arr) {
+    if (counts[num]) {
+      counts[num]++;
+    } else {
+      counts[num] = 1;
+    }
+  }
+
+  let highestCount = 0;
+
+  for (const num of arr) {
+    const count = counts[num];
+    if (count >= 3 && count > highestCount) {
+      highestCount = count;
+    }
+    if (count >= 4 && count > highestCount) {
+      highestCount = count;
+    }
+  }
+
+  const sumOfAllDice = arr.reduce((a, b) => a + b, 0);
+
+  if (highestCount >= 4) {
+    updateRadioOption(1, sumOfAllDice);
+  }
+
+  if (highestCount >= 3) {
+    updateRadioOption(0, sumOfAllDice);
+  }
+
+  updateRadioOption(5, 0);
+};
+
+const detectFullHouse = (arr) => {
+  const counts = {};
+
+  for (const num of arr) {
+    counts[num] = counts[num] ? counts[num] + 1 : 1;
+  }
+
+  const hasThreeOfAKind = Object.values(counts).includes(3);
+  const hasPair = Object.values(counts).includes(2);
+
+  if (hasThreeOfAKind && hasPair) {
+    updateRadioOption(2, 25);
+  }
+
+  updateRadioOption(5, 0);
+};
+
+const resetRadioOptions = () => {
+  scoreInputs.forEach((input) => {
+    input.disabled = true;
+    input.checked = false;
+  });
+
+  scoreSpans.forEach((span) => {
+    span.textContent = "";
+  });
+};
+
+const resetGame = () => {
+  diceValuesArr = [0, 0, 0, 0, 0];
+  score = 0;
+  round = 1;
+  rolls = 0;
+
+  listOfAllDice.forEach((dice, index) => {
+    dice.textContent = diceValuesArr[index];
+  });
+
+  totalScoreElement.textContent = score;
+  scoreHistory.innerHTML = "";
+
+  rollsElement.textContent = rolls;
+  roundElement.textContent = round;
+
+  resetRadioOptions();
+};
+
+const checkForStraights = (arr) => {
+  const counts = {};
+  for (const num of arr) {
+    if (counts[num]) {
+      counts[num]++;
+    } else {
+      counts[num] = 1;
+    }
+  }
+
+  const obj = (evenArr) => {
+    const sameNum = {};
+    for (const num of evenArr) {
+      if (sameNum[num]) {
+        sameNum[num]++;
+      } else {
+        sameNum[num] = 1;
+      }
+    }
+    if (Object.keys(sameNum).length === 1) {
+      return true;
+    }
+  };
+
+  if (Object.keys(counts).length === 4) {
+    const numbers = Object.keys(counts).map((el) => Number(el));
+    let x = 3;
+    for (let i = 0; i < 3; i++) {
+      numbers[i] += x;
+      x--;
+    }
+    if (obj(numbers)) {
+      updateRadioOption(3, 30);
+    }
+  }
+
+  if (Object.keys(counts).length === 5) {
+    const numbers = Object.keys(counts).map((el) => Number(el));
+    let x = 4;
+    for (let i = 0; i < 4; i++) {
+      numbers[i] += x;
+      x--;
+    }
+    if (obj(numbers)) {
+      updateRadioOption(3, 30);
+      updateRadioOption(4, 40);
+    }
+  }
+  updateRadioOption(5, 0);
+};
+
 rollDiceBtn.addEventListener("click", () => {
   if (rolls === 3) {
     alert("You have made three rolls this round. Please select a score.");
   } else {
     rolls++;
+    resetRadioOptions();
     rollDice();
     updateStats();
+    getHighestDuplicates(diceValuesArr);
+    detectFullHouse(diceValuesArr);
   }
 });
 
@@ -60,5 +203,34 @@ rulesBtn.addEventListener("click", () => {
   } else {
     rulesBtn.textContent = "Show rules";
     rulesContainer.style.display = "none";
+  }
+});
+
+keepScoreBtn.addEventListener("click", () => {
+  let selectedValue;
+  let achieved;
+
+  for (const radioButton of scoreInputs) {
+    if (radioButton.checked) {
+      selectedValue = radioButton.value;
+      achieved = radioButton.id;
+      break;
+    }
+  }
+
+  if (selectedValue) {
+    rolls = 0;
+    round++;
+    updateStats();
+    resetRadioOptions();
+    updateScore(selectedValue, achieved);
+    if (round > 6) {
+      setTimeout(() => {
+        alert(`Game Over! Your total score is ${score}`);
+        resetGame();
+      }, 500);
+    }
+  } else {
+    alert("Please select an option or roll the dice");
   }
 });
