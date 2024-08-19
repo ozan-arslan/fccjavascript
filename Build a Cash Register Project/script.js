c646810f755ee75563730ab7cf393e52ea7c01cb
ozan-arslan <arslanozan.arb@gmail.com>
2024-08-19 20:53:15 +0300

    I have unintentionally used the first brute force solution cliche. Now, the main calculating function is much more efficient. Having a lot of problems with floating point numbers. The computer is not ignoring epsilons. Hence, there are no dollars, only Japanese Yen, this commit is before the expansion of the hundreds.

--- Build a Cash Register Project/script.js
+++ Build a Cash Register Project/script.js
@@ -5,54 +5,47 @@ const cidCash = document.getElementById("cash-drawer-display");
 const pElements = cidCash.getElementsByTagName("p");
 const pArray = Array.from(pElements);
 
-let price = 187;
+let price = 0;
 let cid = [
-  ["Pennies: $", 101],
-  ["Nickels: $", 205],
-  ["Dimes: $", 310],
-  ["Quarters: $", 425],
-  ["Ones: $", 9000],
-  ["Fives: $", 5500],
-  ["Tens: $", 2000],
-  ["Twenties: $", 6000],
-  ["Hundreds: $", 10000],
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100],
 ];
 
-const cashInCid = cid.map((el) => el[1]);
-const monValues = [1, 5, 10, 25, 100, 500, 1000, 2000, 10000];
-let totalCashInCid = cashInCid.reduce((acc, currentVal) => {
-  return acc + currentVal;
-}, 0);
-let i;
+let cCash;
+const monValues = [0.01, 0.05, 0.1, 0.25, 1, 5, 10, 20, 100];
+const monQuantity = cid.map((el, i) => {
+  return Math.ceil(el[1] / monValues[i]);
+});
 
-const recursive = (cashInCid, changeDue) => {
-  if (changeDue > 0) {
-    if (changeDue >= monValues[i] && cashInCid[i] != 0) {
-      cashInCid[i] -= monValues[i];
-      changeDue -= monValues[i];
-      recursive(cashInCid, changeDue);
-    } else {
-      i--;
-      recursive(cashInCid, changeDue);
+const calculate = (changeDue) => {
+  for (let i = 8; i != -1 && changeDue > 0; i--) {
+    if (changeDue >= monValues[i] && monQuantity[i] != 0) {
+      let billsToGive = (changeDue - (changeDue % monValues[i])) / monValues[i];
+      if (billsToGive <= monQuantity[i]) {
+        monQuantity[i] -= billsToGive;
+        changeDue -= billsToGive * monValues[i];
+        console.log(billsToGive * monValues[i]);
+      } else {
+        changeDue -= monQuantity[i] * monValues[i];
+        monQuantity[i] = 0;
+        console.log(monQuantity[i] * monValues[i]);
+      }
     }
-  } else {
-    return;
+    console.log(monQuantity);
+    console.log(changeDue);
   }
 };
 
-const calculate = () => {
-  let cCash = Number(input.value) * 100;
-  let changeDue = cCash - price;
-  i = 8;
-
-  recursive(cashInCid, changeDue);
-};
-
 const activateBtn = () => {
-  calculate();
-  pArray.forEach((p, index) => {
-    p.textContent = cid[index][0] + cashInCid[index] / 100;
-  });
+  cCash = Number(input.value);
+  calculate(cCash - price);
 };
 
 const isBtnPressed = (e) => {
